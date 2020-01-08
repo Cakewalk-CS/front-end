13d63c0fcb7934cccaba790c107877e95897b338
Lambda_School_Loaner_163 <kimberlyswinton@gmail.com>
2020-01-08 15:21:40 -0500

    :lipstick: Login Styling Midways
    Login Styling Midways

--- adv_project_fe/src/components/Login.js
+++ adv_project_fe/src/components/Login.js
@@ -1,6 +1,30 @@
 import React, {useState} from "react";
 import axios from 'axios'
 import Modal from './GameStartModal'
+import styled from 'styled-components'
+
+const ParentDiv = styled.div `
+display: flex;
+
+
+`
+
+const LoginFormDiv = styled.div `
+display: flex;
+background-color: #1c2630;
+width: 50% ;
+`
+
+const LoginForm = styled.div `
+
+`
+
+const LogoDiv = styled.div `
+background-color: #55dde0;
+width: 50% ;
+height: 800px;
+`
+
 
 const Login = props => {
   const [user, setUser] = useState({
@@ -29,34 +53,41 @@ const Login = props => {
     }
     
   return (
-    <>
+  <ParentDiv>
+    <LogoDiv>
+
+    </LogoDiv>
     <Modal success={success} setSuccess={setSuccess}/>
-        <div className = 'loginFormDiv'>
-        <form className='loginForm' onSubmit={login}>
-        <label>
-            Username
-            <input
-            type="text"
-            name="username"
-            value={user.username}
-            onChange={handleChange}
-            />
-        </label>  
-        <label>
-            Password
-            <input
-            type="password"
-            name="password"
-            value={user.password}
-            onChange={handleChange}
-            />
-        </label>
-        <a>Forgot your password?</a>
-        <button className='loginButton' type='submit'>Sign in</button>
-        </form>
+        <LoginFormDiv>
+          <LoginForm>
+          <form onSubmit={login}>
+            <label>
+                Username
+                <input
+                style={{top: "554px", left: "16px", width: "50%", height: "40px", borderRadius: "6px", borderColor: "#f26419"}}
+                type="text"
+                name="username"
+                value={user.username}
+                onChange={handleChange}
+                />
+            </label>  
+            <label>
+                Password
+                <input
+                style={{top: "554px", left: "16px", width: "50%", height: "40px", borderRadius: "6px", borderColor: "#f26419"}}
+                type="password"
+                name="password"
+                value={user.password}
+                onChange={handleChange}
+                />
+            </label>
+            <a>Forgot your password?</a>
+            <button className='loginButton' type='submit'>Sign in</button>
+          </form>
+          </LoginForm>
         <div className='loginLine'></div>
-    </div>
-    </>
+      </LoginFormDiv>
+  </ParentDiv>
   );
 };
 
