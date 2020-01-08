10d83b068e4e5c7b38ef7f687a92177162e252de
chad kidd <ckiddwebdev@gmail.com>
2020-01-07 18:10:25 -0800

    dependencies and Room component GET request

--- adv_project_fe/src/components/Rooms.js
+++ adv_project_fe/src/components/Rooms.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+// import { Route } from "react-router-dom";
+import axios from 'axios'
 
+class Rooms extends Component {
+  constructor(){
+    super();
+    this.state = {
+      rooms: []
+    };
+  }
 
-function Rooms() {
+  componentDidMount(){
+    axios.get('https://text-adv-game.herokuapp.com/api/adv/rooms')
+    .then(res => {
+      this.setState({ rooms: res.data})
+    })
+    .catch(error=>{
+      console.log(error, "THIS IS AN ERROR")
+    });
+  }
+
+  render(){
   return (
-    <>
-
-    </>
+    <div>
+    <h1>ALL ROOMS</h1>
+    <div>
+      {this.state.rooms.map(currentRoom => (
+        <p key={currentRoom.id}>{currentRoom.room_name}</p>
+      ))}
+      </div>
+    </div>
   );
-}
+}}
 
 export default Rooms;
