3671231f23787de236fd56661cc2184761143dfd
Lambda_School_Loaner_163 <kimberlyswinton@gmail.com>
2020-01-09 05:37:36 -0500

    :art: Login is looking dandy
    Login is looking dandy

--- adv_project_fe/src/components/Login.js
+++ adv_project_fe/src/components/Login.js
@@ -2,27 +2,71 @@ import React, {useState} from "react";
 import axios from 'axios'
 import Modal from './GameStartModal'
 import styled from 'styled-components'
+import ori from "../SVG/ori.svg"
+import title from "../SVG/title.svg"
 
 const ParentDiv = styled.div `
 display: flex;
-
+text-align: center;
 
 `
 
 const LoginFormDiv = styled.div `
 display: flex;
-background-color: #1c2630;
+background-color: #f26419;
 width: 50% ;
+justify-content: center;
+
 `
 
-const LoginForm = styled.div `
+// const LoginForm = styled.div `
+// display: flex;
 
+// text-align: center;
+// `
+
+const Form = styled.form `
+margin-top: 200px;
+
+`;
+
+
+const UsrLabel = styled.label `
+font-family: 'Exo 2', sans-serif;
+font-weight:bold;
+font-size:20px;
+
+`
+const PwLabel = styled.label `
+font-family: 'Exo 2', sans-serif;
+font-weight:bold;
+font-size:20px;
+`
+
+const Input =styled.input `
+font-size: 20px;
+`
+const LButton = styled.button `
+border-radius: 20px;
+border-color: transparent;
+background-color: #efb22d;
+cursor: pointer;
+width: 80%
+margin: 0 auto;
+margin-top:25px;
+font-family: 'Exo 2', sans-serif;
+font-weight:bold;
+font-size:20px;
+height:40px;
+&:hover{
+  background: #33658a;
+}
 `
 
 const LogoDiv = styled.div `
 background-color: #55dde0;
 width: 50% ;
-height: 800px;
+height: 720px;
 `
 
 
@@ -55,36 +99,37 @@ const Login = props => {
   return (
   <ParentDiv>
     <LogoDiv>
-
+    <img style={{marginTop:"30px", marginRight:"15px"}} src={title} alt="title" />;
+    <img style={{width:"90%"}} src={ori} alt="Ori" />;
     </LogoDiv>
     <Modal success={success} setSuccess={setSuccess}/>
         <LoginFormDiv>
-          <LoginForm>
-          <form onSubmit={login}>
-            <label>
-                Username
-                <input
-                style={{top: "554px", left: "16px", width: "50%", height: "40px", borderRadius: "6px", borderColor: "#f26419"}}
+          {/* <LoginForm> */}
+          <Form onSubmit={login}>
+            <UsrLabel>
+                USERNAME
+                <Input
+                style={{top: "554px", left: "16px", width: "100%", height: "40px", borderRadius: "20px", borderColor: "#f26419"}}
                 type="text"
                 name="username"
                 value={user.username}
                 onChange={handleChange}
                 />
-            </label>  
-            <label>
-                Password
-                <input
-                style={{top: "554px", left: "16px", width: "50%", height: "40px", borderRadius: "6px", borderColor: "#f26419"}}
+            </UsrLabel>  
+            <PwLabel>
+                PASSWORD
+                <Input
+                style={{top: "554px", left: "16px", width: "100%", height: "40px", borderRadius: "20px", borderColor: "#f26419"}}
                 type="password"
                 name="password"
                 value={user.password}
                 onChange={handleChange}
                 />
-            </label>
-            <a>Forgot your password?</a>
-            <button className='loginButton' type='submit'>Sign in</button>
-          </form>
-          </LoginForm>
+            </PwLabel>
+            <div style={{font: 'Exo 2', fontSize: "20px"}}>Forgot your password?</div>
+            <LButton className='loginButton' type='submit'>SIGN IN</LButton>
+          </Form>
+          {/* </LoginForm> */}
         <div className='loginLine'></div>
       </LoginFormDiv>
   </ParentDiv>
